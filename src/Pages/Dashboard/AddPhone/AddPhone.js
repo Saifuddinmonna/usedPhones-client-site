9cb8d15197bf6774f228cec6b24472d265e622a0
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-11-27 03:44:42 +0600

    add phone use query added successfully

--- src/Pages/Dashboard/AddPhone/AddPhone.js
+++ src/Pages/Dashboard/AddPhone/AddPhone.js
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { data } from "autoprefixer";
 import React from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
@@ -30,12 +31,7 @@ const AddPhone = () => {
 			return data;
 		},
 
-		// queryKey: ["divisions"],
-		// queryFn: async () => {
-		// 	const res = await fetch("http://localhost:5000/divisionsname");
-		// 	const data = await res.json();
-		// 	return data;
-		// },
+		
 	});
 	const {
 		data: divisions,
@@ -50,13 +46,9 @@ const AddPhone = () => {
 			return data;
 		},
 
-		// queryKey: ["divisions"],
-		// queryFn: async () => {
-		// 	const res = await fetch("http://localhost:5000/divisionsname");
-		// 	const data = await res.json();
-		// 	return data;
-		// },
+		
 	});
+	console.log(data.location);
 
 	const handleAddPhone = (data) => {
 		const image = data.image[0];
@@ -84,8 +76,10 @@ const AddPhone = () => {
 						originalPrice: data.originalPrice,
 						resalePrice: data.resalePrice,
 						yearOfUse: data.yearOfUse,
+						dateOfBuying : data.dateOfBuying,
 						timeOfPost: new Date("2015-03-25"),
-						multipleImage: data.multipleImage,
+						location : data.location,
+						multipleImage: data.multipleImage
 					};
 
 					// save phone information to the database
@@ -114,139 +108,219 @@ const AddPhone = () => {
 	}
 
 	return (
-		<div className="w-96 p-7">
-			<h2 className="text-4xl">Add A Phone</h2>
-			<form onSubmit={handleSubmit(handleAddPhone)}>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">Name</span>
-					</label>
-					<input
-						type="text"
-						{...register("name", {
-							required: "Name is Required",
-						})}
-						className="input input-bordered w-full max-w-xs"
-					/>
-					{errors.name && (
-						<p className="text-red-500">{errors.name.message}</p>
-					)}
-				</div>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">Email</span>
-					</label>
-					<input
-						type="email"
-						{...register("email", {
-							required: true,
-						})}
-						className="input input-bordered w-full max-w-xs"
-					/>
-					{errors.email && (
-						<p className="text-red-500">{errors.email.message}</p>
-					)}
-				</div>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">Original Price</span>
-					</label>
-					<input
-						type="number"
-						{...register("originalPrice", {
-							required: true,
-						})}
-						className="input input-bordered w-full max-w-xs"
-					/>
-					{errors.email && (
-						<p className="text-red-500">{errors.email.message}</p>
-					)}
-				</div>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">Resale Price</span>
-					</label>
-					<input
-						type="number"
-						{...register("resalePrice", {
-							required: true,
-						})}
-						className="input input-bordered w-full max-w-xs"
-					/>
-					{errors.email && (
-						<p className="text-red-500">{errors.email.message}</p>
-					)}
-				</div>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">Year of Use</span>
-					</label>
-					<input
-						type="date"
-						{...register("yearOfUse", {
-							required: true,
-						})}
-						className="input input-bordered w-full max-w-xs"
-					/>
-					{errors.email && (
-						<p className="text-red-500">{errors.email.message}</p>
-					)}
-				</div>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">brand</span>
-					</label>
-					<select
-						{...register("brand")}
-						className="select input-bordered w-full max-w-xs">
-						{brands.map((brand) => (
-							<option key={brand._id} value={brand.brand}>
-								{brand.brand}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">Location</span>
-					</label>
-					<select
-						{...register("location")}
-						className="select input-bordered w-full max-w-xs">
-						{divisions.map((division) => (
-							<option key={division._id} value={division.division}>
-								{division.division}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="form-control w-full max-w-xs">
-					<label className="label">
-						{" "}
-						<span className="label-text">Photo</span>
-					</label>
-					<input
-						type="file"
-						multiple
-						accept="image/*"
-						{...register("image", {
-							required: "Photo is Required",
-						})}
-						className="input input-bordered w-full max-w-xs"
-					/>
+		<div className="flex flex-col justify-center items-center mt-5 ">
+			<div className="px-7 ">
+				<h2 className="text-4xl text-center">Add A Phone</h2>
+			</div>
+			<div className="flex flex-row justify-center items-center mx-5 my-6 ">
+				<form
+					className="text-center grid  justify-center items-center"
+					onSubmit={handleSubmit(handleAddPhone)}>
+					<div className="text-center grid gap-4 md:grid-cols-2 lg:grid-cols-2  justify-center items-center">
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">Seller Name</span>
+							</label>
+							<input
+								type="text"
+								{...register("name", {
+									required: "Name is Required",
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.name && (
+								<p className="text-red-500">
+									{errors.name.message}
+								</p>
+							)}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">
+									Condition Type
+								</span>
+							</label>
+							<input
+								type="text"
+								{...register("condition", {
+									required: "Name is Required",
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.name && (
+								<p className="text-red-500">
+									{errors.name.message}
+								</p>
+							)}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">Seller Email</span>
+							</label>
+							<input
+								type="email"
+								{...register("email", {
+									required: true,
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.email && (
+								<p className="text-red-500">
+									{errors.email.message}
+								</p>
+							)}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">
+									Original Price
+								</span>
+							</label>
+							<input
+								type="number"
+								{...register("originalPrice", {
+									required: true,
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.email && (
+								<p className="text-red-500">
+									{errors.email.message}
+								</p>
+							)}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">
+									Mobile Number
+								</span>
+							</label>
+							<input
+								type="number"
+								{...register("mobileNumber", {
+									required: true,
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.email && (
+								<p className="text-red-500">
+									{errors.email.message}
+								</p>
+							)}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">Resale Price</span>
+							</label>
+							<input
+								type="number"
+								{...register("resalePrice", {
+									required: true,
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.email && (
+								<p className="text-red-500">
+									{errors.email.message}
+								</p>
+							)}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">
+									Date of buying
+								</span>
+							</label>
+							<input
+								type="date"
+								{...register("dateOfBuying", {
+									required: true,
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.email && (
+								<p className="text-red-500">
+									{errors.email.message}
+								</p>
+							)}
+							<label className="label">
+								{" "}
+								<span className="label-text">Year of Use</span>
+							</label>
+							<input
+								type="number"
+								{...register("yearOfUse", {
+									required: true,
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
+							{errors.email && (
+								<p className="text-red-500">
+									{errors.email.message}
+								</p>
+							)}
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">brand</span>
+							</label>
+							<select
+								{...register("brand")}
+								className="select input-bordered w-full max-w-xs">
+								{brands.map((brand) => (
+									<option key={brand._id} value={brand.brand}>
+										{brand.brand}
+									</option>
+								))}
+							</select>
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">Location</span>
+							</label>
+							<select
+								{...register("location")}
+								className="select input-bordered w-full max-w-xs">
+								{divisions.map((division) => (
+									<option
+										key={division._id}
+										value={division.division}>
+										{division.division}
+									</option>
+								))}
+							</select>
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								{" "}
+								<span className="label-text">Photo</span>
+							</label>
+							<input
+								type="file"
+								multiple
+								accept="image/*"
+								{...register("image", {
+									required: "Photo is Required",
+								})}
+								className="input input-bordered w-full max-w-xs"
+							/>
 
-					{errors.img && (
-						<p className="text-red-500">{errors.img.message}</p>
-					)}
-				</div>
-				{/* <div className="form-control w-full max-w-xs">
+							{errors.img && (
+								<p className="text-red-500">
+									{errors.img.message}
+								</p>
+							)}
+						</div>
+						{/* <div className="form-control w-full max-w-xs">
 					<label className="label">
 						{" "}
 						<span className="label-text">Photo</span>
@@ -271,12 +345,17 @@ const AddPhone = () => {
 						<p className="text-red-500">{errors.img.message}</p>
 					)}
 				</div> */}
-				<input
-					className="btn btn-accent w-full mt-4"
-					value="Add phone"
-					type="submit"
-				/>
-			</form>
+					</div>
+
+					<div>
+						<input
+							className="btn btn-primary w-full  mt-4"
+							value="Add phone"
+							type="submit"
+						/>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
